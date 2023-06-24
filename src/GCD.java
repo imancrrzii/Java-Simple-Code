50baacf0bc359d8a544a5c73872eeb10215a737b
imancrrzii <imancarrazi777@gmail.com>
2023-06-24 11:41:15 +0700

    Modify GCD.java to OOP

--- src/GCD.java
+++ src/GCD.java
@@ -1,19 +1,33 @@
 import java.util.Scanner;
 
 public class GCD {
+    private int n1;
+    private int n2;
 
-  public static void main(String[] args) {
-    Scanner userInput = new Scanner(System.in);
-    System.out.println("Masukkan n1 = ");
-    int n1 = userInput.nextInt();
-    System.out.println("Masukkan n2 = ");
-    int n2 = userInput.nextInt();
-
-    int gcd = 1;
-
-    for (int i = 1; i <= n1 && i <= n2; ++i) {
-      if (n1 % i == 0 && n2 % i == 0) gcd = i;
+    public GCD(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
     }
-    System.out.println("GCD = " + gcd);
-  }
+
+    public int calculateGCD() {
+        int gcd = 1;
+        for (int i = 1; i <= n1 && i <= n2; ++i) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Masukkan n1: ");
+        int n1 = userInput.nextInt();
+        System.out.print("Masukkan n2: ");
+        int n2 = userInput.nextInt();
+
+        GCD gcdCalculator = new GCD(n1, n2);
+        int gcd = gcdCalculator.calculateGCD();
+        System.out.println("GCD = " + gcd);
+    }
 }
