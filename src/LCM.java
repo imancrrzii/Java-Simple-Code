59f5e565c5e3dc6c4e81a1acbdd1e975fd8d1481
imancrrzii <imancarrazi777@gmail.com>
2023-06-24 11:45:05 +0700

    Modify LCM.java to OOP

--- src/LCM.java
+++ src/LCM.java
@@ -1,20 +1,40 @@
 import java.util.Scanner;
 
 public class LCM {
-    
+    private int n1;
+    private int n2;
+
+    public LCM(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+    public int calculateLCM() {
+        int gcd = calculateGCD();
+        return (n1 * n2) / gcd;
+    }
+
+    private int calculateGCD() {
+        int gcd = 1;
+
+        for (int i = 1; i <= n1 && i <= n2; ++i) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                gcd = i;
+            }
+        }
+
+        return gcd;
+    }
+
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Masukkan n1 = ");
+        System.out.print("Masukkan n1: ");
         int n1 = userInput.nextInt();
-        System.out.println("Masukkan n2 = ");
+        System.out.print("Masukkan n2: ");
         int n2 = userInput.nextInt();
-    
-        int gcd = 1;
-    
-        for (int i = 1; i <= n1 && i <= n2; ++i) {
-          if (n1 % i == 0 && n2 % i == 0) gcd = i;
-        }
-        int lcm = (n1 * n2) / gcd;
-        System.out.println("LCM = " + lcm);
-      }
+
+        LCM lcm = new LCM(n1, n2);
+        int result = lcm.calculateLCM();
+        System.out.println("LCM = " + result);
     }
+}
